4651df78b6da0814453cb9ea82cab73f86b5c1e8
Benji Copan <benji1220@gmail.com>
2021-06-27 12:11:01 -0500

    Migrated a lot of functionality back to client.js. Previously, I was POSTing each individual character to the server. Now I only POST when user hits =

--- server/public/client.js
+++ server/public/client.js
@@ -1,36 +1,91 @@
 $( document ).ready(onReady);
 
 function onReady() {
-    $('#numpad').on("click",".calculatorButton", buttonPush);
-    $('#clearHistory').on("click",clearHistory);
+    $( "#numpad" ).on("click",".calculatorButton", buttonPush);
+    $( "#equation" ).on("change", function( event ) {
+        equationGlobal = $( "#equation" ).val();
+        console.log(equationGlobal);
+    })
+    $( "#setOrderOfOperations" ).on("click", toggleOrderOfOperations)
+    $( "#clearHistory" ).on("click",clearHistory);
 
 } //end onReady
 
-    //remaining functions listed alphabetically
+//globals initialized
+let equationGlobal = "";
+let orderGlobal = "leftToRight";
+
+//remaining functions listed alphabetically
 
 function buttonPush() {
-    console.log(this.id);
+    // get button function (listed in id after "_" )
     let button = this.id.substring(this.id.indexOf("_")+1);
     console.log(button);
-
-    // send to server
-    $.ajax({
-        //type
-        method: 'POST',
-        url: '/calculate',
-        data: {
-            button: button
-        } //data becomes req.body on server
-    })
-    .then( function(response) {
-        // successful send case
-        console.log('posted item', response);
-    })
-    .catch( function(err) {
-        console.log('failed to post', err);
-    })
+    switch (button) {
+        case "equals":
+            // send to server
+            $.ajax({
+                //type
+                method: 'POST',
+                url: '/calculate',
+                data: {
+                    equation: equationGlobal,
+                    order: orderGlobal
+                } //data becomes req.body on server
+            })
+            .then( function(response) {
+                // successful send case
+                console.log('posted item', response);
+            })
+            .catch( function(err) {
+                console.log('failed to post', err);
+            });
+            equationGlobal="";
+            $( "#equation" ).val(equationGlobal);
+            break;
+        case "plus":
+            equationGlobal += " + ";
+            $( "#equation" ).val(equationGlobal);
+            break;
+        case "minus":
+            equationGlobal += " - ";
+            $( "#equation" ).val(equationGlobal);
+            break;
+        case "times":
+            equationGlobal += " * ";
+            $( "#equation" ).val(equationGlobal);
+            break;
+        case "divide":
+            equationGlobal += " / ";
+            $( "#equation" ).val(equationGlobal);
+            break;
+        case "decimal":
+            equationGlobal += ".";
+            $( "#equation" ).val(equationGlobal);
+            break;
+        case "clear":
+            equationGlobal = "";
+            $( "#equation" ).val(equationGlobal);
+            break;                 
+        default:
+            equationGlobal += button;
+            $( "#equation" ).val(equationGlobal);
+            break;
+    }
+    
 } //end buttonPush
 
 function clearHistory() {
     
 } //end clearHistory
+
+function toggleOrderOfOperations() {
+    const el = $('#setOrderOfOperations');
+    if (el.html() === "Evaluate left to right") {
+        el.html("Follow PEMDAS");
+        orderGlobal = "PEMDAS";
+    } else {
+        el.html("Evaluate left to right");
+        orderGlobal = "leftToRight";
+    }
+} //end toggleOrderOfOperations
