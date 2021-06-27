7aaf22e8996944883db775cbe139871fb14b3d1c
Benji Copan <benji1220@gmail.com>
2021-06-26 19:49:40 -0500

    established server POST communication

--- server/public/client.js
+++ server/public/client.js
@@ -2,8 +2,35 @@ $( document ).ready(onReady);
 
 function onReady() {
     $('#numpad').on("click",".calculatorButton", buttonPush);
+    $('#clearHistory').on("click",clearHistory);
+
 } //end onReady
+
+    //remaining functions listed alphabetically
 
 function buttonPush() {
     console.log(this.id);
-}
+    let button = this.id.substring(this.id.indexOf("_")+1);
+    console.log(button);
+
+    // send to server
+    $.ajax({
+        //type
+        method: 'POST',
+        url: '/calculate',
+        data: {
+            button: button
+        } //data becomes req.body on server
+    })
+    .then( function(response) {
+        // successful send case
+        console.log('posted item', response);
+    })
+    .catch( function(err) {
+        console.log('failed to post', err);
+    })
+} //end buttonPush
+
+function clearHistory() {
+    
+} //end clearHistory
