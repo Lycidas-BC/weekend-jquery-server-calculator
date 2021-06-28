f964e32330b36ade35b2e865707a02d85caa547a
Benji Copan <benji1220@gmail.com>
2021-06-27 19:49:27 -0500

    Added PEMDAS functionality - additional testing needed

--- server/server.js
+++ server/server.js
@@ -114,7 +114,6 @@ function evaluate(equation, order) {
     //check for 
     //I'm incrementing through the array backwards, because I plan on splicing things out and I don't want to mess up the array indices
     while (indexFromBack >= 0) {
-        console.log("element, element is operation", equationArray[indexFromBack], equationArray[indexFromBack].search(/[+_*/]/) > -1);
         if (equationArray[indexFromBack] === "-" && equationArray[indexFromBack+1] === "-") {
             //if we have two negatives in a row, splice them out
             console.log('double negative:',equationArray, equationArray[indexFromBack], equationArray[indexFromBack+1]);
@@ -122,20 +121,16 @@ function evaluate(equation, order) {
                 //if there's a number before and after the double negative, replace it with a +
                 equationArray[indexFromBack] = "+";
                 equationArray.splice(indexFromBack+1, 1);
-                console.log('splice double negative:', equationArray);
             } else {
                 //otherwise, just splice out the double negative
                 equationArray.splice(indexFromBack,2);
-                console.log('splice double negative:', equationArray);
             }
         } else if (equationArray[indexFromBack].search(/[-+_*/]/) > -1 && equationArray[indexFromBack+1].search(/[-+_*/]/) > -1) {
             //check for two operations in a row
             if (equationArray[indexFromBack+1] === "-") {
                 //if the second one is "-", make the next value negative and splice it out
                 equationArray[indexFromBack+2] = "-" + equationArray[indexFromBack+2];
-                console.log(equationArray);
                 equationArray.splice(indexFromBack+1,1);
-                console.log(equationArray);
             } else {
                 return "error: two operations in a row"
             }
@@ -156,8 +151,6 @@ function evaluate(equation, order) {
     let operation = "";
     if (order === "leftToRight"){
         for (const index in equationArray){
-            console.log("runningTotal", runningTotal, "operation", operation);
-            console.log(parseFloat(equationArray[index]), equationArray[index]);
             if (index == 0) {
                 runningTotal = parseFloat(equationArray[0]);
             } else if (equationArray[index].search(/[+_*/]/ || equationArray[index] === "-") > -1){
@@ -166,32 +159,71 @@ function evaluate(equation, order) {
                 switch (operation) {
                     case "+":
                         runningTotal += parseFloat(equationArray[index]);
-                        console.log("runningTotal",runningTotal);
                         break;
                     case "-":
                         runningTotal -= parseFloat(equationArray[index]);
-                        console.log("runningTotal",runningTotal),"-";
                         break;
                     case "*":
                         runningTotal *= parseFloat(equationArray[index]);
-                        console.log("runningTotal",runningTotal),"*";
                         break;
                     case "/":
                         if (parseFloat(equationArray[index]) === 0) {
                             return "error: divide by 0";
                         }
                         runningTotal /= parseFloat(equationArray[index]);
-                        console.log("runningTotal",runningTotal),"/";
                         break;
                     default:
-                        console.log("runningTotal",runningTotal),"d";
                         break;
                 }
             }
         }
     } //end leftToRight case
     else {
-        return "Sorry, PEMDAS order of operations is not yet available in your version. Make sure you're using the most up-to-date version of the software";
+        let count = 0;
+        while(equationArray.length > 1 && count < 10) {
+            console.log(equationArray);
+            const nextMult = equationArray.findIndex(element => element === "*");
+            const nextDiv = equationArray.findIndex(element => element === "/");
+            const nextAdd = equationArray.findIndex(element => element === "+");
+            const nextSub = equationArray.findIndex(element => element === "-");
+            console.log("nextMult",nextMult,"NextDiv",nextDiv,"NextAdd",nextAdd,"NextSub",nextSub)
+            if (nextMult > -1 || nextDiv > -1) {
+                //if there is multiplication or division, do those first
+                console.log("AAA", nextMult, nextDiv, nextMult != -1, parseFloat(equationArray[nextMult-1]),parseFloat(equationArray[nextMult+1]));
+                if ((nextMult < nextDiv || nextDiv == -1) && nextMult != -1) {
+                    //if multiplication is left of division or if there is no division, multiply numbers and splice them into array
+                    const product = parseFloat(equationArray[nextMult-1]) * parseFloat(equationArray[nextMult+1]);
+                    equationArray.splice(nextMult-1,3,product);
+                } else {
+                    //either there is no multiplication or it's after the next division. Divide numbers and splice them into array
+                    if (parseFloat(equationArray[nextDiv+1]) == 0) {
+                        return "error: divide by 0";
+                    }
+                    const quotient = parseFloat(equationArray[nextDiv-1]) / parseFloat(equationArray[nextDiv+1]);
+                    console.log(quotient);
+                    equationArray.splice(nextMult-1,3,quotient);
+                    console.log(equationArray);
+                }
+            } else if (nextAdd > -1 || nextSub > -1) {
+                //once we've done all multiplication and division, do addition and subtraction from left to right
+                if ((nextAdd < nextSub || nextSub == -1) && nextAdd != -1) {
+                    //if addition is left of subtraction or there is no subtraction, add and splice
+                    const sum = parseFloat(equationArray[nextAdd-1]) + parseFloat(equationArray[nextAdd+1]);
+                    console.log(sum);
+                    equationArray.splice(nextAdd-1,3,sum);
+                    console.log(equationArray);
+                } else {
+                    //either there is no addition or it's after the next subtraction. Subtract and splice
+                    const difference = parseFloat(equationArray[nextSub-1]) - parseFloat(equationArray[nextSub+1]);
+                    console.log(difference);
+                    equationArray.splice(nextSub-1,3,difference);
+                    console.log(equationArray);
+                }
+            }
+            count +=1;
+        }
+        runningTotal = equationArray[0];
+        //return "Sorry, PEMDAS order of operations is not yet available in your version. Make sure you're using the most up-to-date version of the software";
     } //end PEMDAS case
     // round to 10 digits precision
     return Math.round(runningTotal*10000000000)/10000000000;
